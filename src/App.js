71bc0dbdf6eb07c0fb464b614898926dd33a11b1
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-13 17:20:07 +0200

    refactors App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import Paint from "./components/Paint";
-import WindowSize from "./components/WindowSize";
+import React, { useState, useEffect } from 'react'
+import Name from './components/Name';
+import Canvas from './components/Canvas'
+import ColorPicker from './components/ColorPicker'
+import WindowSize from './components/WindowSize'
+import randomColor from 'randomcolor'
 
-function App() {
+
+export default function Paint() {
+  const [colors, setColors] = useState([])
+  const [activeColor, setActiveColor] = useState(null)
+  const getColors = () => {
+    const baseColor = randomColor().slice(1);
+    fetch(`https://www.thecolorapi.com/scheme?hex=${baseColor}&mode=monochrome`)
+    .then(res => res.json())
+    .then(res => {
+      setColors(res.colors.map(color => color.hex.value))
+      setActiveColor(res.colors[0].hex.value)
+    })
+  }
+  useEffect(getColors, [])
+  
   return (
-    <div className="App">
-      <Paint />
+    <div className="app">
+      <header style={{ borderTop: `10px solid ${activeColor}` }}>
+        <div>
+          <Name />
+        </div>
+        <div style={{ marginTop: 10 }}>
+          <ColorPicker
+            colors={colors}
+            activeColor={activeColor}
+            setActiveColor={setActiveColor}
+          />
+        </div>
+      </header>
+      {activeColor && (
+        <Canvas
+          color={activeColor}
+          height={window.innerHeight}
+        />
+      )}
       <WindowSize />
-
     </div>
-  );
+  )
 }
-
-export default App;
