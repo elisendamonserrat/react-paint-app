c368f19a46134382ad8f069a956c144b0a206a11
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-08-31 08:46:29 +0200

    Adds Name component - creates a controlled input form to set the name of the project

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 
 export default function Name() {
-  const [ name, setName ] = useState("");
-    return (
-        <label className="header-name">
-          <input 
-            value={name}
-            onChange={e => setName(e.target.value)}
-            onClick={e => e.target.setSelectionRange(0, e.target.value.length)}
-            placeholder="Untitled Canvas"
-          />
-        </label>
-    )
+  const [name, setName] = useState('');
+  return (
+    <label className="header-name" for="project-title">
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        onClick={(e) => e.target.setSelectionRange(0, e.target.value.length)}
+        placeholder={"Add your project title here"}
+      />
+    </label>
+  )
 }
-
